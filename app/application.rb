bc1afe6e43d7bf12286c8571ff6684010bfdd7f6
Jeff Swisher <jefftswisher@gmail.com>
2020-01-22 18:52:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.length == 0 
         resp.write "Your cart is empty"
-      elsif
+      else
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
